50a51a1f58bcdded947a461a7e983fd86bbd06c0
João Ribeiro <Joao.PP.Ribeiro@ctw.bmwgroup.com>
2021-10-02 21:21:34 +0100

    Implemented cli argument to define which day to run

--- main.go
+++ main.go
@@ -3,8 +3,22 @@ package main
 import (
 	"aoc-2020/day2"
 	"fmt"
+	"os"
 )
 func main() {
-	fmt.Println("Starting...")
-	day2.Solution()
+	if len(os.Args) <= 1 {
+		fmt.Println("Pass a day as argument: day<num>")
+		os.Exit(1)
+	}
+
+	arg := os.Args[1]
+
+	switch arg {
+	case "day1":
+		day2.Solution()
+	case "day2":
+		day2.Solution()
+	default:
+		fmt.Println("Pass a day as argument: day<num>")
+	}
 }
