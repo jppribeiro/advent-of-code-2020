0127baf3e0f13e49e64e51f108bc0cf5e23b56e5
João Ribeiro <Joao.PP.Ribeiro@ctw.bmwgroup.com>
2021-10-03 12:29:16 +0100

    Implemented day4 part 2

--- internal/day4/day4.go
+++ internal/day4/day4.go
@@ -4,18 +4,59 @@ import (
 	"aoc-2020/internal/utils"
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
-var mandatoryFields = []string{
-	"byr",
-	"iyr",
-	"eyr",
-	"hgt",
-	"hcl",
-	"ecl",
-	"pid",
-	// "cid",
+/*
+byr (Birth Year) - four digits; at least 1920 and at most 2002.
+iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+hgt (Height) - a number followed by either cm or in:
+If cm, the number must be at least 150 and at most 193.
+If in, the number must be at least 59 and at most 76.
+hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+pid (Passport ID) - a nine-digit number, including leading zeroes.
+cid (Country ID) - ignored, missing or not.
+ */
+
+type height struct {
+	cm func(val int) bool
+	in func(val int) bool
+}
+
+type rules struct {
+	byr func(val int) bool
+	iyr func(val int) bool
+	eyr func(val int) bool
+	hgt height
+	hcl string
+	ecl string
+	pid string
+}
+
+var validator = rules{
+	func(val int) bool {
+		return val >= 1920 && val <= 2002
+	},
+	func(val int) bool {
+		return val >= 2010 && val <= 2020
+	},
+	func(val int) bool {
+		return val >= 2020 && val <= 2030
+	},
+	height{
+		func(val int) bool {
+			return val >= 150 && val <= 193
+		},
+		func(val int) bool {
+			return val >= 59 && val <= 76
+		},
+	},
+	"^#[a-f0-9]{6,6}$",
+	"^(amb|blu|brn|gry|grn|hzl|oth)$",
+	"^[0-9]{9}$",
 }
 
 func Solution() {
@@ -24,7 +65,10 @@ func Solution() {
 	passports := extractPassports(lines)
 
 	fmt.Println(":: Part1 ::")
-	fmt.Printf("There are %d valid passports.\n\n", checkValid(passports))
+	fmt.Printf("There are %d valid passports.\n\n", bulkCheckValid(passports))
+
+	fmt.Println(":: Part2 ::")
+	fmt.Printf("There are %d valid passports.\n\n", checkValidB(passports))
 }
 
 func extractPassports(rawData []string) []string {
@@ -54,24 +98,96 @@ func extractPassports(rawData []string) []string {
 	return passports
 }
 
-func checkValid(passports []string) int {
+func bulkCheckValid(passports []string) int {
 	num := 0
 
 	for _, p := range passports {
-		if len(strings.Split(p, " ")) < 7 {
-			continue
-		}
-
-		if len(strings.Split(p, " ")) == 8 {
-			num++
-			continue
-		}
-
-		if match, _ := regexp.MatchString("cid", p); !match {
-			// If there is 1 field missing and it is NOT cdi then consider valid
+		if checkValid(p) {
 			num++
 		}
 	}
 
 	return num
+}
+
+func checkValid(p string) bool {
+	if len(strings.Split(p, " ")) < 7 {
+		return false
+	}
+
+	if len(strings.Split(p, " ")) == 8 {
+		return true
+	}
+
+	if match, _ := regexp.MatchString("cid", p); !match {
+		// If there is 1 field missing and it is NOT cdi then consider valid
+		return true
+	}
+
+	return false
+}
+
+func checkValidB(passports []string) int {
+	numValid := 0
+
+	for _, p := range passports {
+		isValid := true
+
+		if !checkValid(p) {
+			continue
+		}
+
+		for _, field := range strings.Split(p, " ") {
+			key := strings.Split(field, ":")[0]
+
+			switch key {
+			case "byr":
+				isValid = isValid && executeValidator(validator.byr, strings.Split(field, ":")[1])
+			case "iyr":
+				isValid = isValid && executeValidator(validator.iyr, strings.Split(field, ":")[1])
+			case "eyr":
+				isValid = isValid && executeValidator(validator.eyr, strings.Split(field, ":")[1])
+			case "hgt":
+				unit := strings.Split(field, ":")[1][len(strings.Split(field, ":")[1])-2:]
+
+				value := strings.Split(field, ":")[1][0:len(strings.Split(field, ":")[1])-2]
+
+				if unit == "cm" {
+					isValid = isValid && executeValidator(validator.hgt.cm, value)
+					break
+				} else if unit == "in" {
+					isValid = isValid && executeValidator(validator.hgt.in, value)
+					break
+				}
+
+				isValid = false
+			case "hcl":
+				match, _ := regexp.MatchString(validator.hcl, strings.Split(field, ":")[1])
+				isValid = isValid && match
+			case "ecl":
+				match, _ := regexp.MatchString(validator.ecl, strings.Split(field, ":")[1])
+				isValid = isValid && match
+			case "pid":
+				match, _ := regexp.MatchString(validator.pid, strings.Split(field, ":")[1])
+				isValid = isValid && match
+			}
+
+			// No need to check more fields when one is not valid
+			if !isValid {
+				fmt.Printf("%s is not valid %s for field %s\n", key, strings.Split(field, ":")[1], field)
+				break
+			}
+		}
+
+		if isValid { numValid++ }
+	}
+
+	return numValid
+}
+
+func executeValidator(cb func(val int) bool, val string) bool {
+	value, err := strconv.Atoi(val)
+	if err != nil { return false }
+
+	return cb(value)
 }
--- internal/day4/day4_test.go
+++ internal/day4/day4_test.go
@@ -68,8 +68,72 @@ func Test_checkValid(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := checkValid(tt.args.passports); got != tt.want {
-				t.Errorf("checkValid() = %v, want %v", got, tt.want)
+			if got := bulkCheckValid(tt.args.passports); got != tt.want {
+				t.Errorf("bulkCheckValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_executeValidator(t *testing.T) {
+	testCb := func(val int) bool {
+		return val >= 1920 && val <= 2001
+	}
+
+	okVal := "1921"
+	notOkVal := "1919"
+
+	type args struct {
+		cb  func(val int) bool
+		val string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Expect true for value in range", args{testCb, okVal}, true},
+		{"Expect true for value in range", args{testCb, notOkVal}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := executeValidator(tt.args.cb, tt.args.val); got != tt.want {
+				t.Errorf("executeValidator() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_checkValidB(t *testing.T) {
+	invalidInputs := []string{
+		"eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+		"iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946",
+		"hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+		"hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007",
+	}
+
+	validInputs := []string{
+		"pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f",
+		"eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+		"hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022",
+		"iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+	}
+
+	type args struct {
+		passports []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Return no valid passports", args{invalidInputs}, 0},
+		{"Return 4 valid passports", args{validInputs}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkValidB(tt.args.passports); got != tt.want {
+				t.Errorf("checkValidB() = %v, want %v", got, tt.want)
 			}
 		})
 	}
